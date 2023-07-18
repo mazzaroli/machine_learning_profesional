1ecdd94d74b118b869b2da8077d03d9afcc2711f
carlos mazzaroli <carlitosmazaroli@gmail.com>
2023-07-18 11:04:38 -0300

    first commit

---
+++ pca.py
@@ -0,0 +1,65 @@
+# Importamos las bibliotecas necesarias
+import pandas as pd  # Para el manejo de datos estructurados
+import sklearn  # Para machine learning en Python
+import matplotlib.pyplot as plt  # Para la visualización de gráficos
+
+# Importamos las clases específicas de scikit-learn
+from sklearn.decomposition import PCA, IncrementalPCA, KernelPCA  # Para análisis de componentes principales
+from sklearn.linear_model import LogisticRegression  # Para regresión logística
+from sklearn.preprocessing import StandardScaler  # Para estandarizar características
+from sklearn.model_selection import train_test_split  # Para dividir los datos en entrenamiento y prueba
+
+# Iniciamos la ejecución del programa principal
+if __name__ == "__main__":
+    df_heart = pd.read_csv('./data/heart.csv') # Se lee el archivo CSV 'heart.csv' y se guarda en el DataFrame df_heart
+    
+    # print(df_heart.head(5)) # Se imprime las primeras 5 filas del DataFrame df_heart
+
+    df_features = df_heart.drop(['target'], axis=1) # Se crea un nuevo DataFrame df_features eliminando la columna 'target' del DataFrame df_heart
+    
+    df_target = df_heart['target'] # Se crea una Serie df_target con los valores de la columna 'target' del DataFrame df_heart
+
+    df_features = StandardScaler().fit_transform(df_features) # Se estandarizan las características del DataFrame df_features utilizando la clase StandardScaler
+
+    # Se dividen los datos en conjuntos de entrenamiento y prueba utilizando train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(df_features,df_target,test_size=0.3,random_state=42)
+
+    # print(X_train.shape)
+    # print(y_train.shape)
+
+    #n_components = min(n_muestras, n_features)
+    pca = PCA(n_components=4)  
+    pca.fit(X_train)  
+
+    # Realizamos el análisis de componentes principales incremental (IPCA)
+    ipca = IncrementalPCA(n_components=4, batch_size=10)  
+    ipca.fit(X_train)  
+
+    # Realizamos el análisis de componentes principales utilizando KernelPCA (KPCA)
+    kpca = KernelPCA(n_components=4, kernel='poly')  
+    kpca.fit(X_train)  
+
+    # Graficamos la varianza explicada por cada componente principal utilizando Matplotlib
+    plt.plot(range(len(pca.explained_variance_)), pca.explained_variance_ratio_)  
+    plt.savefig('a')  
+
+    # Creamos un clasificador de regresión logística
+    logistic = LogisticRegression(solver='lbfgs')
+
+    # Transformamos los datos de entrenamiento y prueba utilizando PCA
+    df_train = pca.transform(X_train)  
+    df_test = pca.transform(X_test)  
+    logistic.fit(df_train, y_train)  
+    print("SCORE PCA:", logistic.score(df_test, y_test))  
+
+    # Transformamos los datos de entrenamiento y prueba utilizando IPCA
+    df_train = ipca.transform(X_train)  
+    df_test = ipca.transform(X_test)  
+    logistic.fit(df_train, y_train)  
+    print("SCORE IPCA:", logistic.score(df_test, y_test))  
+
+    # Transformamos los datos de entrenamiento y prueba utilizando KPCA
+    df_train = kpca.transform(X_train)  
+    df_test = kpca.transform(X_test)  
+    logistic.fit(df_train, y_train)  
+    print("SCORE KPCA:", logistic.score(df_test, y_test))
---
+++ regularization.py
@@ -0,0 +1,68 @@
+# Importamos las bibliotecas necesarias
+import pandas as pd  # Para el manejo de datos estructurados
+
+from sklearn.linear_model import LinearRegression, Lasso, Ridge, ElasticNet
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
+if __name__ == "__main__":
+    dataset = pd.read_csv('./data/whr2017.csv')
+    
+    # Separar las características (X) y la variable objetivo (y)
+    X = dataset[['gdp', 'family', 'lifexp', 'freedom', 'generosity', 'corruption', 'dystopia']]
+    y = dataset[['score']]
+
+    # Dividir el conjunto de datos en conjuntos de entrenamiento y prueba
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+
+    # Entrenar los modelos de regresión lineal, Lasso, Ridge y ElasticNet
+    model_lineal = LinearRegression().fit(X_train, y_train)
+    model_lasso = Lasso(alpha=0.02).fit(X_train, y_train)
+    model_ridge = Ridge(alpha=1).fit(X_train, y_train)
+    model_elastic = ElasticNet(alpha=0.01).fit(X_train, y_train)
+    
+    # Realizar predicciones en el conjunto de prueba
+    y_predict_lineal  = model_lineal.predict(X_test)
+    y_predict_lasso   = model_lasso.predict(X_test)
+    y_predict_ridge   = model_ridge.predict(X_test)
+    y_predict_elastic = model_elastic.predict(X_test)
+
+    # Calcular el MSE para cada modelo
+    lineal_loss = mean_squared_error(y_test, y_predict_lineal)
+    lasso_loss = mean_squared_error(y_test, y_predict_lasso)
+    ridge_loss = mean_squared_error(y_test, y_predict_ridge)
+    elastic_loss = mean_squared_error(y_test, y_predict_elastic)
+
+    # Imprimir los valores de MSE para cada modelo
+    print('lineal_loss: ', lineal_loss)
+    print('lasso_loss:  ', lasso_loss)
+    print('ridge_loss:  ', ridge_loss)
+    print('elastic_loss:', elastic_loss, '\n')
+
+    # Crear un DataFrame con los coeficientes de cada modelo
+    data = pd.DataFrame(columns=['lineal', 'lasso', 'ridge', 'elastic'])
+    data['lineal'] = model_lineal.coef_.reshape(-1)
+    data['lasso'] = model_lasso.coef_.reshape(-1)
+    data['ridge'] = model_ridge.coef_.reshape(-1)
+    data['elastic'] = model_elastic.coef_.reshape(-1)
+    
+    # Establecer los índices del DataFrame como las columnas de entrada X
+    data.index = X.columns
+    
+    # Imprimir los coeficientes de cada modelo
+    print(data)
+
+
+# lineal_loss:  9.893337283086869e-08
+# lasso_loss:   0.049605751139829145
+# ridge_loss:   0.005650124499962814
+# elastic_loss: 0.00912409728272294
+
+#               lineal     lasso     ridge   elastic
+# gdp         1.000128  1.289214  1.072349  1.106542
+# family      0.999946  0.919694  0.970486  0.962883
+# lifexp      0.999835  0.476864  0.856054  0.803020
+# freedom     1.000034  0.732973  0.874002  0.861674
+# generosity  1.000260  0.142455  0.732857  0.654667
+# corruption  0.999771  0.000000  0.685833  0.554539
+# dystopia    0.999938  0.899653  0.962066  0.953720
